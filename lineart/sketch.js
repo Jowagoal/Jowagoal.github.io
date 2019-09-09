6bfbacbfc6a5171d9b6d60117ca04f37bb0184d7
Jowagoal <54913022+Jowagoal@users.noreply.github.com>
2019-09-09 14:11:42 -0600

    LineArt

---
+++ lineart/sketch.js
@@ -0,0 +1,15 @@
+// Line Art Demo
+// Jordie Walter
+// Sept 9, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+line(mouseX,mouseY,pmouseX,pmouseY);
+}
