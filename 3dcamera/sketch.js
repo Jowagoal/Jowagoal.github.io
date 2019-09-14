07b2766afb8edfc80fd0871c933cc751819ba596
Jowagoal <54913022+Jowagoal@users.noreply.github.com>
2019-09-14 08:36:47 -0600

    More tests

--- 3dcamera/sketch.js
+++ 3dcamera/sketch.js
@@ -1,15 +1,22 @@
-// Line Art Demo
+// 3d Test
 // Jordie Walter
 // Sept 9, 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let numCubes = 0;
-let cam;
+let numCubesX = 0;
+let numCubesY = 0;
+let numCubesZ = 0;
+
+
+
 let xCentre;
 let yCentre;
 let zCentre;
+
+let cam;
+
 let camX=0;
 let camY=0;
 let camZ=0;
@@ -25,39 +32,60 @@ function setup() {
 function draw() {
   background(220)
   orbitControl();
-  makeCubes();
+  makeCubesX();
+  makeCubesZ();
+  makeCubesY();
   lookAtCubes();
 }
 
 function lookAtCubes(){
-  xCentre=numCubes*60/2
+  xCentre=numCubesX*60/2
   while(camX<=xCentre){
-    console.log(camX);
     cam.lookAt(camX,camY,camZ);
     camX++;
     //slow down frame rate or move camera slower
   }
 }
 
-function rotation(){
-  if(keyIsDown(32)){
-    console.log('space pressed');
-    rotateY(mouseX*0.01);
-    rotateX(mouseY*-0.01);
-  }
-}
-function keyPressed(){
-  if(keyIsDown(39)){
-    numCubes +=1;
-  }
-  if(keyIsDown(37)){
-    numCubes -=1;
+
+function makeCubesX(){
+  for(let i=0; i<=numCubesX; i++){
+    box(50);
+    translate(60,0,0);
   }
 }
 
-function makeCubes(){
-  for(let i=0; i<=numCubes; i++){
+function makeCubesZ(){
+  for(let j=0; j<numCubesZ; j++){
     box(50);
-    translate(60,0,0);
+    translate(0,0,-60);
+  }
+}
+
+function makeCubesY(){
+  for(let k=0; k<numCubesY; k++){
+    box(50);
+    translate(0,-60,0);
+  }
+}
+
+function keyPressed(){
+  if(keyIsDown(39)){
+    numCubesX+=1;
+  }
+  if(keyIsDown(37)){
+    numCubesX-=1;
+  }
+  if(keyIsDown(38)){
+    numCubesZ+=1;
+  }
+  if(keyIsDown(40)){
+    numCubesZ-=1;
+  }
+  if(keyIsDown(32)){
+    numCubesY+=1;
+  }
+  if(keyIsDown(8)){
+    numCubesY-=1;
   }
 }
