e188e76af6b0374da02f05eda9043321da124b0e
Jowagoal <54913022+Jowagoal@users.noreply.github.com>
2019-10-03 21:43:30 -0600

    Massive update
    -collision with snake added
    -food added
    -death screen added
    -better collision with walls
    -snake color scheme changed

--- 3dcoordinatetest/sketch.js
+++ 3dcoordinatetest/sketch.js
@@ -4,24 +4,50 @@
 //
 // Extra for Experts:
 
+let x = 800;
+let y = 300;
+let z = -500;
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  fill(255,0,0);
-
+  
 }
 
 function draw() {
+  fill(0,255,0,20);
   background(220);
   orbitControl();
-  
-  boxAt(50,0,0);
-  boxAt(50,0,0);
-  boxAt(50,0,0);
-}
 
-function boxAt(x,y,z){
+  strokeWeight(1);
+  stroke(2);
   box(50);
-  translate(x,y,z);
+  translate(50,0,0);
+  box(50);
+  translate(50,0,0);
+  box(50);
+  translate(50,0,0);
+  box(50);
+  translate(0,0,-50);
+  box(50);
+  translate(0,0,-50);
+  box(50);
+  translate(0,0,-50);
+  box(50);
+  translate(0,50,0);
+  box(50);
+  translate(0,50,0);
+  box(50);
+  translate(0,50,0);
+  box(50);
+
+  fill(255,0,0);
+  translate(500,0,0);
+  sphere(25,25,25);
+  translate(-500,0,0);
+
+  fill(0,255,0,20);
+  translate(0,50,0);
+  box(50);
 }
 
 function windowResized(){
--- snake/sketch.js
+++ snake/sketch.js
@@ -9,20 +9,21 @@ let state = "Menu";
 
 let arr = [0,0,0,0];
 let position = [0,0,0];
-let positionCube = [];
+let foodPosition = [0,0,0];
+let bodyPosition = [];
+let positionStored = [0,0,0];
+
 let push0 = 50;
 let push1 = 0;
 let push2 = 0;
 let push3 = 1;
 let snakeLength = 3;
 
-let cam1;
-
 let inconsolata;
 let instructions = ["Controls:", "A                   = left", "D                   = right", "W                  = forward", "S                   = back", "Up Arrow      = up", "Down Arrow =  down"];
-let toggle = 1;
 
-//preloads the font
+let gameCounter = 0;
+
 function preload(){
   inconsolata = loadFont('assets/Inconsolata.otf');
 }
@@ -34,24 +35,27 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     textSize(25);
   }else if(state==="Play"){
-    //creates a 3d canvas
     createCanvas(windowWidth, windowHeight, WEBGL);
     
     camera(-300,-400,600,500,700,-500);
-
-    frameRate(10);
-  }else if(state==="Game Over"){
     
+    frameRate(10);
+    
+    foodPosition[0]=ceil(random(0,19))*50;
+    foodPosition[1]=ceil(random(0,19))*50;
+    foodPosition[2]=ceil(random(-19,0))*50;
+  }else if(state==="Game Over"){
+    createCanvas(windowWidth, windowHeight);
   }
 }
 
-//draw loop calls all functions
 function draw() {
   checkState();
 }
 
 function checkState(){
   if(state==="Menu"){
+    resetAllValues();
     startScreen();
   }
   if(state==="Options"){
@@ -61,16 +65,20 @@ function checkState(){
     gamePlay();
   }
   if(state==="Game Over"){
-    
+    gameCounter=1;
+    deathScreen();
   }
 }
 
 function startScreen(){
+  if(gameCounter===1){
+    translate(-1/2*width,-1/2*height);
+  }
   background(220);
   rectMode(CENTER);
   textAlign(CENTER, CENTER);
   textSize(25);
-
+  
   fill(255);
   rect(width/2, height/2, width/4, height/8);
   fill(0);
@@ -80,7 +88,7 @@ function startScreen(){
   rect(width/2, height/2+height/4, width/4, height/8);
   fill(0);
   text("Options",width/2, height/2+height/4);
-
+  
   
   if(mouseX>width/2-width/8&&mouseX<width/2+width/8&&mouseY>height/2-height/16&&mouseY<height/2+height/16&&mouseIsPressed){
     state="Play";
@@ -91,11 +99,33 @@ function startScreen(){
   }
 }
 
-function optionMenu(){
-  background(220);
-  controls();
-  fill(255,0,0);
+function resetAllValues(){
+  arr = [0,0,0,0];
+  position = [0,0,0];
+  foodPosition = [0,0,0];
+  bodyPosition = [];
+  positionStored = [0,0,0];
 
+  push0 = 50;
+  push1 = 0;
+  push2 = 0;
+  push3 = 1;
+  snakeLength = 3;
+}
+
+function optionMenu(){
+  if(gameCounter===1){
+    translate(-1/2*width,-1/2*height);
+  }
+  background(220);
+  textAlign(LEFT, TOP);
+  fill(0);
+  for(var i=0; i<=instructions.length; i++){
+    text(instructions[i], 100, 100);
+    translate(0, 25);
+  }
+
+  fill(255,0,0);
   rectMode(CENTER);
   rect(width*0.9,-150, 30, 20);
   if(mouseX>width*0.9-15&&mouseX<width*0.9+15&&mouseY>38&&mouseY<60&&mouseIsPressed){
@@ -109,9 +139,9 @@ function gamePlay(){
   
   strokeWeight(5);
   createBoard();
-
+  
   gameStartedWaitTime();
-
+  
   strokeWeight(2);
   gameStart();
 }
@@ -122,19 +152,19 @@ function createBoard(){
   line(-25,-25,25,975,-25,25);
   line(-25,-25,25,-25,975,25);
   line(-25,-25,25,-25,-25,-975);
-
+  
   line(975,975,-975,975,975,25);
   line(975,975,-975,975,-25,-975);
   line(975,975,-975,-25,975,-975);
-
+  
   line(-25,975,25,975,975,25);
   line(-25,975,25,-25,975,-975);
   
   line(975,-25,-975,975,-25,25);
   line(975,-25,-975,-25,-25,-975);
-
+  
   line(-25,-25,-975,-25,975,-975);
-
+  
   line(975,-25,25,975,975,25);
 }
 
@@ -146,48 +176,69 @@ function gameStart(){
   stroke(0);
   orbitControl();
   
-  readArray();
-  
-  position[0]=position[0]+push0;
-  position[1]=position[1]+push1;
-  position[2]=position[2]+push2;
+  food();
   
   arr.push(push0);
   arr.push(push1);
   arr.push(push2);
   arr.push(push3);
   
-  if(position[0]<-100||position[0]>950||position[1]<0||position[1]>1000||position[2]>0||position[2]<-1050){
-    state = "Game Over";
-  }
+  moveSnake();
 }
 
-//reads avery 5 indexs of the array
-function readArray(){
+function moveSnake(){
+  position[0]=0;
+  position[1]=0;
+  position[2]=0;
   for(var i=0; i<=arr.length; i+=4){
-    //translates by the first 3 indexs of 5
     translate(arr[i],arr[i+1],arr[i+2]);
-    //creates a box if the fourth index is a 1
+    if(arr[i+0]===50||arr[i+0]===-50||arr[i+1]===50||arr[i+1]===-50||arr[i+2]===50||arr[i+2]===-50){
+      position[0]=position[0]+arr[i+0];
+      position[1]=position[1]+arr[i+1];
+      position[2]=position[2]+arr[i+2];
+    }
+    if(position[0]<0||position[0]>950||position[1]<0||position[1]>950||position[2]>0||position[2]<-950){
+      state = "Game Over";
+      setup();
+    }
     if(arr[i+3]===1){
       placeBox();
-      //fill(0,200,0, 50);
-      //box(50);
     }
     if(arr[i-(4*snakeLength-3)]===1){
       arr[i-(4*snakeLength-3)]=0;
     }
   }
+
+  bodyPosition.push(positionStored[0]);
+  bodyPosition.push(positionStored[1]);
+  bodyPosition.push(positionStored[2]);
+  
+  positionStored.splice(0,3);
+
+  positionStored.push(position[0]);
+  positionStored.push(position[1]);
+  positionStored.push(position[2]);
+  
+  if(bodyPosition.length>snakeLength*3){
+    bodyPosition.splice(0,3);
+  }
+  for(var j=0; j<=bodyPosition.length-3; j+=3){
+    if(position[0]===bodyPosition[j]&&position[1]===bodyPosition[j+1]&&position[2]===bodyPosition[j+2]){
+      
+      state = "Game Over";
+      setup();
+    }
+  }
 }
 
-//borders are -100<x>900, 0<y>1000, -1000<z>0
 function placeBox(){
   let x = position[0];
   let y = position[1];
   let z = position[2];
-  if(x<=-100||x>=900||y<=0||y>=1000||z<=-1000||z>=0){
-  fill(255,0,0,50);
+  if(x<=0||x>=950||y<=0||y>=950||z<=-950||z>=0){
+    fill(255,0,0,50);
     box(50);
-  }else if(x<=0||x>=800||y<=100||y>=900||z<=-900||z>=-100){
+  }else if(x<=100||x>=850||y<=100||y>=850||z<=-850||z>=-100){
     fill(0,0,255,50);
     box(50);
   }
@@ -197,10 +248,53 @@ function placeBox(){
   }
 }
 
-//when a key is pressed this function is called
+function food(){
+  if(position[0]===foodPosition[0]&&position[1]===foodPosition[1]&&position[2]===foodPosition[2]){
+    foodPosition[0]=ceil(random(0,19))*50;
+    foodPosition[1]=ceil(random(0,19))*50;
+    foodPosition[2]=ceil(random(-19,0))*50;
+    snakeLength++;
+  }else{
+
+    let x = foodPosition[0];
+    let y = foodPosition[1];
+    let z = foodPosition[2];
+    fill(255,0,0);
+    noStroke();
+    translate(x, y, z);
+    box(50);
+    translate(-1*x, -1*y, -1*z);
+    stroke(2);
+  }
+}
+
+function deathScreen(){
+  textFont(inconsolata);
+  translate(-1/2*width,-1/2*height);
+  background(220);
+  textAlign(CENTER, CENTER);
+  
+  fill(255,0,0);
+  textSize(50);
+  text("You Died!", width/2, height/8);
+  
+  fill(0);
+  textSize(25);
+  text("Score: " + snakeLength, width/2, height/4);
+
+  fill(255);
+  rectMode(CENTER);
+  rect(width/2,height/2+height/8, width/4, height/8);
+
+  fill(0);
+  text("Back to Menu", width/2, height/2+height/8);
+  if(mouseX>width/2-width/8&&mouseX<width/2+width/8&&mouseY>height/2+height/8-height/16&&mouseY<height/2+height/8+height/16&&mouseIsPressed){
+    state="Menu";
+    setup();
+  }
+}
+
 function keyPressed(){
-  //if 'd' is pressed it pushes 5-bits into the array
-  //telling it to move the ball to the right 
   if(keyIsDown(68)){
     if(push0!==-50){
       push0=50;
@@ -209,8 +303,6 @@ function keyPressed(){
     push2=0;
     position[0]=position[0]+50;
   }else if(keyIsDown(65)){
-  //if 'a' is pressed it pushes 5-bits into the array
-  //telling it to move the ball to the left
     if(push0!==50){
       push0=-50;
     }
@@ -218,8 +310,6 @@ function keyPressed(){
     push2=0;
     position[0]=position[0]-50;
   }else if(keyIsDown(87)){
-    //if 'w' is pressed it pushes 5-bits into the array
-    //telling it to move the ball forward
     push0=0;
     push1=0;
     if(push2!==50){
@@ -227,8 +317,6 @@ function keyPressed(){
     }
     position[2]=position[2]-50;
   }else if(keyIsDown(83)){
-    //if 's' is pressed it pushes 5-bits into the array
-    //telling it to move the ball backward 
     push0=0;
     push1=0;
     if(push2!==-50){
@@ -236,8 +324,6 @@ function keyPressed(){
     }
     position[2]=position[2]+50;
   }else if(keyIsDown(38)){
-    //if 'UP_ARROW' is pressed it pushes 5-bits into the array
-    //telling it to move the ball up 
     push0=0;
     if(push1!==50){
       push1=-50;
@@ -245,8 +331,6 @@ function keyPressed(){
     push2=0;
     position[1]=position[1]-50;
   }else if(keyIsDown(40)){
-    //if 'DOWN_ARROW' is pressed it pushes 5-bits into the array
-    //telling it to move the ball down 
     push0=0;
     if(push1!==-50){
       push1=50;
@@ -256,18 +340,6 @@ function keyPressed(){
   }
 }
 
-
-//shows the controls if it is toggeled on
-function controls(){
-  textAlign(LEFT, TOP);
-  fill(0);
-  for(var i=0; i<=instructions.length; i++){
-    text(instructions[i], 100, 100);
-    translate(0, 25);
-  }
-}
-
-//when window is resized setup is called again
 function windowResized(){
   setup();
 }
