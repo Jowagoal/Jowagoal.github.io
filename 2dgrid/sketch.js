98d15f7628a14b154d1247e408d4ce0d4b35a388
Jowagoal <54913022+Jowagoal@users.noreply.github.com>
2019-10-29 14:12:30 -0600

    Skins in snake started

--- 2dgrid/sketch.js
+++ 2dgrid/sketch.js
@@ -6,71 +6,148 @@
 // - describe what you did to take this project "above and beyond"
 
 let grid;
-let totalRows = 20;
-let totalCols = 20;
+let rows = 30;
+let cols = 30;
+let autoPlay = false;
 
 function setup() {
-  if(windowWidth>windowHeight){
-    createCanvas(windowHeight,windowHeight);
-  }else{
-    createCanvas(windowWidth,windowWidth);
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
   }
-  background(220);
-  
-  grid = create2dGrid(totalRows,totalCols);
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+  grid = createRandom2dArray(cols, rows);
 }
 
 function draw() {
-  display(grid);
-}
-
-function create2dGrid(rows, cols){
-  let grid2D = [];
-  for(let i=0; i<rows; i++){
-    let thisGrid = [];
-    for(let j=0; j<cols; j++){
-      if(random(100)>50){
-        thisGrid.push(1);
-      }else{
-        thisGrid.push(0);
-      }
-    }
-    grid2D.push(thisGrid);
-  }
-  return grid2D;
-}
-
-function display(grid){
-  for(let i=0; i<totalRows; i++){
-    for(let j=0; j<totalCols; j++){
-      if(grid[i][j]===0){
-        fill(0);
-      }else{
-        fill(255);
-      }
-      rect(i*width/totalRows, j*height/totalCols, 
-           width/totalRows, height/totalRows);
+  background(220);
+  displayGrid(grid, rows, cols);
+  if (autoPlay) {
+    if (frameCount % 5 === 0) {
+      update();
     }
   }
 }
 
-function mouseClicked(){
-  let x = mouseX;
-  let y = mouseY;
-  for(let i=0; i<totalRows; i++){
-    for(let j=0; j<totalCols; j++){
-      if(x>i*width/totalRows&&x<i*width/totalRows+width/totalRows+1&&
-         y>j*height/totalCols&&y<j*height/totalCols+height/totalCols+1){
-        if(grid[i][j]===0){
-          grid[i][j]=1;
-        }else{
-          grid[i][j]=0;
+function windowResized() {
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+}
+
+function keyTyped() {
+  if (key === "r") {
+    grid = createRandom2dArray(cols, rows);
+  }
+  if (key === "c") {
+    grid = createEmptyGrid();
+  }
+  if (key === " ") {
+    update();
+  }
+  if (key === "a") {
+    autoPlay = !autoPlay;
+  }
+}
+
+function mousePressed() {
+  let cellSize = width/cols;
+
+  let xCoord = floor(mouseX / cellSize);
+  let yCoord = floor(mouseY / cellSize);
+  
+  if (grid[yCoord][xCoord] === 1) {
+    grid[yCoord][xCoord] = 0;
+  }
+  else {
+    grid[yCoord][xCoord] = 1;
+  }
+}
+
+function createEmptyGrid() {
+  let emptyGrid = [];
+  for (let x = 0; x < cols; x++) {
+    emptyGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      emptyGrid[x].push(0);
+    }
+  }
+  return emptyGrid;
+}
+
+function update() {
+  let nextTurn = createEmptyGrid();
+
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      let neighbors = 0;
+
+      //loop around the neighbor spots...
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          //deal with edge cases
+          if (x+i >= 0 && x+i < cols && y+j >= 0 && y+j < rows) {
+            neighbors += grid[y+j][x+i];
+          }
+        }
+      }
+      //don't count self as a neighbor
+      neighbors -= grid[y][x];
+
+      //apply rules!
+      if (grid[y][x] === 1) { //currently alive
+        if (neighbors === 2 || neighbors === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+
+      if (grid[y][x] === 0) { //currently dead
+        if (neighbors === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
         }
       }
     }
   }
+  grid = nextTurn;
 }
 
-function windowResized(){
-  setup();
+function displayGrid(grid, rows, cols) {
+  let cellSize = width / cols;
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
+        fill(255);
+      }
+      else {
+        fill(0);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function createRandom2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      if (random(100) < 50) {
+        randomGrid[x].push(1);
+      }
+      else {
+        randomGrid[x].push(0);
+      }
+    }
+  }
+  return randomGrid;
 }
