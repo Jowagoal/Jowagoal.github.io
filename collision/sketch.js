0f31dc60886e3f27c9a05e13421af1d508dc7494
Jowagoal <54913022+Jowagoal@users.noreply.github.com>
2020-06-02 17:19:28 -0600

    some realistic bouncing

--- collision/sketch.js
+++ collision/sketch.js
@@ -3,8 +3,8 @@ let things = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   fill(0);
-  things.push(new Thing(width/2,height/2,20,20));
-  things.push(new Thing(width/2+10,0,20,20));
+  things.push(new Thing(width/2,height*0,20,20,100));
+  things.push(new Thing(width/2,height*1/2,20,20,200));
 }
 
 function draw(){
@@ -12,7 +12,7 @@ function draw(){
   for(var i=things.length-1; i>=0; i--){
     for(var j=i-1; j>=0; j--){
       if(things[i].x===things[j].x){
-        things[i].x+=1;
+        //things[i].x+=1;
       }
       if(dist(things[i].x,things[i].y,things[j].x,things[j].y)<=things[i].r+things[j].r){
         let hold = {
@@ -34,7 +34,7 @@ function draw(){
 }
 
 class Thing{
-  constructor(x,y,radius,mass){
+  constructor(x,y,radius,mass,c){
     this.x = x;
     this.y = y;
     this.r =  radius;
@@ -42,36 +42,81 @@ class Thing{
     this.dx = 0;
     this.dy = 0;
     this.g = 0.2;
+    this.a = PI+HALF_PI;
+    this.s = 0;
+    this.c = c
   }
 
   interact(thi){
-    let o = this.y-thi.y;
-    let h = dist(this.x,this.y,thi.x,thi.y);
-    let a = abs(asin(o/h));
-    let e = Math.pow(a/HALF_PI,3);
-    this.dy = thi.dy*e;
-    this.dx = thi.dy*(e-1);
+    let cA = abs(asin((this.y-thi.y)/(dist(this.x,this.y,thi.x,thi.y))));
+    let rL = cA;
+    let AoR = abs(this.a-rL*2);
+    this.a = AoR;
+    this.dy = this.s*sin(this.a);
+    this.dx = -this.s*cos(this.a);
   }
 
   move(){
-    if(this.y>=height-this.r){
-      this.dy*=-0.8;
-      this.y = height-this.r;
-    }
-    if(!abs(this.dy)<this.g||this.y<=height-this.r-this.g){
-      this.dy+=this.g;
-    }else{
-      this.dy = 0;
-      this.y = height-this.r;
-    }
-    this.y+=this.dy;
-    this.x+=this.dx;
+    //MOVE
+      //bounce
+      if(this.y>=height-this.r){
+        this.dy*=-0.8;
+        this.dx*=0.8;
+        this.y = height-this.r;
+      }
+      if(this.x>=width-this.r){
+        this.dx*=-0.8;
+        this.dy*=0.8;
+        this.x = width-this.r;
+      }
+      if(this.x<=0+this.r){
+        this.dx*=-0.8;
+        this.dy*=0.8;
+        this.x = 0+this.r;
+      }
+      //gravity
+      if(!abs(this.dy)<this.g||this.y<=height-this.r-this.g){
+        this.dy+=this.g;
+      //stop bounce
+      }else{
+        this.dy = 0;
+        this.y = height-this.r;
+      }
+      this.y+=this.dy;
+      this.x+=this.dx;
+
+    //CONVERT
+      this.a = atan(this.dy/this.dx);
+      if(this.dy<0){
+        if(this.dx<0){
+          this.a = PI-this.a;
+        }else{
+          this.a*=-1;
+        }
+      }else{
+        if(this.dx<0){
+          this.a = PI+this.a*-1;
+        }else{
+          this.a = TWO_PI-this.a;
+        }
+      }
+      if(this.a>=TWO_PI){
+        this.a-TWO_PI;
+      }
+      this.s = this.dy/sin(this.a);
+
+    //NEW
+    this.dy = this.s*sin(this.a);
+    this.dx = -this.s*cos(this.a);
   }
+  
   display(){
+    fill(this.c)
     circle(this.x,this.y,this.r*2);
   }
 }
 
 function mouseClicked(){
-  things.push(new Thing(mouseX,mouseY,20,20))
+  things.push(new Thing(mouseX,mouseY,20,20,random(0,255)))
+  //frameRate(1);
 }
